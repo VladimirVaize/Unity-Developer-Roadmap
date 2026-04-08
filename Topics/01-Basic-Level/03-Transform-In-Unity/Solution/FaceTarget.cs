daadb665f801104dca06795b96456edd8e0c1584
Vladimir Vaize <50355961+VladimirVaize@users.noreply.github.com>
2026-04-09 00:19:19 +1000

    Create FaceTarget.cs

---
+++ Topics/01-Basic-Level/03-Transform-In-Unity/Solution/FaceTarget.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FaceTarget : MonoBehaviour
+{
+    private GameObject _target;
+    private void Awake()
+    {
+        _target = GameObject.Find("Target");
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(_target.transform.position - transform.position);
+    }
+}
